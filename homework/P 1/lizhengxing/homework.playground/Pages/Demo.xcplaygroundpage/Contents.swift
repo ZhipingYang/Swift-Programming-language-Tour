a2e3b7ff3f12917021cc87c6835a2e0ee20967a4
李正兴 <lizhengxing@chelun.com>
2017-03-10 10:13:56 +0800

    homework for lizhengxing

---
+++ homework/P 1/lizhengxing/homework.playground/Pages/Demo.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ 🤜 Demo 🤛
+ ------
+ > 如下图，实现一个数据列表，只要可支持张开关闭状态即可，数据存储方式不使用Class来实现，需要创建一个新project将其实现。
+ 存储方式：
+ - 使用Struct实现
+ - 使用enum 实现
+ 
+ ![图片](WechatIMG4.jpeg)
+ */
+
+
+//: [基础](@previous)
